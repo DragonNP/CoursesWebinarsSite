4568059ccd8a09ace31db33376a21773ff52b70a
Никита Погудалов <dragonnp@ya.ru>
2023-08-11 00:15:12 +0300

    move allowed hosts to .env file

--- courses_webinars/courses_webinars/settings.py
+++ courses_webinars/courses_webinars/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.0', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = [config('ALLOWED_HOSTS_1'), config('ALLOWED_HOSTS_2'), config('ALLOWED_HOSTS_3')]
 
 # Application definition
 
